8b78fec00e0282004f3eefd562cd6509ef2ea561
Brian Takita <brian.takita@gmail.com>
2014-05-25 21:48:43 -0700

    Added existence-is-god.mm

---
+++ contents/articles/existence-is-god/index.mm
@@ -0,0 +1,163 @@
+Existence is God {
+  This is an attempt to define a spiritual system with culturally true definitions. {
+    This is intended to be a living iterative document meant to interact with current culture.
+    Discussion is encouraged. The language is self-referential.
+  }
+  All definitions apply to the definitions in this document. {
+    Each section within this document is an Entity.  
+  
+  }
+  Document Resolution {
+    Root Meaning of the three terms (This definition is inspired by the similarity of their root lower resolutions.) {
+      The nature of information means it is a system and it has a soul.
+      
+    }
+    Intention of Resolution { The intention of being at a lower resolution is to highlight the sameness of these terms. }
+    At higher conceptual resolutions { Information, System, & Soul have nuanced & different meanings. }
+  
+  }
+  Existence/God {
+    Everything that "is", or more simply, everything (Physical, Conceptual, Social, Spirit). The overarching system.
+    
+  }
+  Information/Entity/System/Being/Soul/Culture/Philosophy/Religion {
+    That which can distinguish one thing from another.
+    Information which exhibits intelligence.
+    Entity { "http://en.wikipedia.org/wiki/Entity" }
+    System {
+      Physical System { "http://en.wikipedia.org/wiki/Physical_system" }
+      Conceptual System { "http://en.wikipedia.org/wiki/Conceptual_system" }
+    }
+    Soul { "http://en.wikipedia.org/wiki/Soul"
+      A Soul is a Being.
+      Animism { "http://en.wikipedia.org/wiki/Animism"
+        I favor Animism''s view of Soul.
+        It is defined by primitive humans, had an undistilled perspective.
+        Their view of souls recognizes the nature of intelligence in it''s primitive forms.
+        Example { Non-biological entities (such as rivers and mountains) possess souls. }
+        Cultural Ego {
+          My view is the Ourstory of Animism is: {
+            Animism is an earlier state of common worldview that has been opposed contemporary culture. {
+              The 1819 view of culture with the Amimism philosophy 
+              
+            }
+          
+          }
+        }
+      }
+    }
+    Being { "http://en.wikipedia.org/wiki/Being" }
+  }
+  Intelligence {
+    The cause/effect system of the Soul.
+    It also include control logic, information transfer, evolution.
+    Human-like Intelligence {
+      logic, abstract thought, understanding, self-awareness, communication, learning, emotional knowledge, memory, planning, and problem solving.
+      
+    }
+  }
+  State {
+    The current status of the information.  
+      
+  }
+  Pattern {
+    The elements of a pattern repeat in a predictable or regular manner.
+    Any of the five traditionally accepted senses may directly observe patterns.
+    Abstract patterns in science, spirit, mathematics, or language require conceptual & extrasensory senses.
+  }
+  Direct Observation {
+    Direct observation in practice means observing socially accepted senses.
+    This includes socially accepted senses within a different reality.  
+    These observations are widespread in nature and in art.
+  }
+  Energy {
+    Energy is a property of objects, transferable among them via fundamental interactions, which can be converted in form but not created or destroyed.
+    The ability of a system (physical/conceptual) to do work on other systems (physical/conceptual)
+  }
+  Spirit {
+    The motivation of the soul in it''s current state.
+    The Holy Spirit causes people to vibrate at a higher level and perform actions with increased zeal.
+    The Spirit of Greed causes one to act in greedy ways.
+  }
+  Reality {
+    Plural { Realities }
+    The interpretation of existence from a soul''s perspective.
+    Reality can be seen as being singular (absolute), or being plural (relative).
+    e.g. A rock does not have a nervous system or much of a dynamic intelligence. It''s reality is more singular  
+    Reality can be socially defined 
+    Can be used to enforce one''s reality to another.
+    Often user to refer to a, The state of things as they actually exist, rather than as they may appear or might be imagined
+    Singular Reality {
+      An absolute view of Existence. From the perspective, Existence cannot be any other way.
+      
+    }
+    Plural Reality {
+      A reality that accepts the existence of and can incorporate other realities.
+      
+    }
+    Being Aware of other Realities has the quality to allows a Soul to live within that particular Reality.
+  }
+  Perspective/Abstraction {
+    The view of reality from a soul''s point of view
+    Abstraction {
+      The view of reality from a Model''s point of view
+      Characteristic Traits {
+        Abstraction is to take a distance from the physical world.
+        Abstraction is (or is a close to) generalization.
+        Abstraction is information hiding.
+        Abstraction is to keep only relevant aspects.
+        Abstraction is a kind of reformulation or approximation.
+        Abstraction is a to change levels of detail.
+      }
+      Model { The Characteristic Traits of abstraction can then be used to build up a Model. }
+      Link { "http://www.aaai.org/ocs/index.php/SARA/SARA13/paper/download/7249/6267" }
+    }
+  }
+  Resolution {
+    The detail & scope of the perspective.
+    Detail is increased with iteration & meditation.
+    A broad perspective on a system usually means a low resolution to be consciously aware of the information.
+    One can focus perspective to gain a higher resolution.
+    You can zoom in & out of the system to observe details & patterns.
+    Fractal {
+      Resolution is increased when zooming into a Fractal.
+      Resolution is decreased when zooming out of a Fractal.
+    }
+    First Order Resolution {
+      Does not include another Resolution.
+      
+    }
+    Second Order Resolution {
+      A Resolution that consists of multiple Resolutions.
+      Often not distinguished by a Resolution, thus seen as a property of a common Resolution.
+      
+    }
+  }
+  Story/Ourstory/History/Herstory {
+    Recounting of a sequence of events/Narrative {
+      A Story is algorithmic.
+      It is often of a low resolution, thus requiring little information to communicate (or transfer) a Conceptual System.
+      It can be iterated upon to create more resolution.
+    }
+  }
+  Tool {
+    Conceptual or Physical System that is used to create a desired effect by another System.
+    
+  }
+  Fractal {
+    A curve or geometric figure, each part of which has the same statistical character as the whole.
+    Fractals are useful in modeling structures (such as eroded coastlines or snowflakes) in which similar patterns: {
+      Recur at progressively smaller scales
+      In describing partly random or chaotic phenomena such as: {
+        Crystal growth
+        Fluid turbulence
+        Galaxy formation
+      }
+    }
+  }
+}
+Notes {
+  All human conceptual experience into one cohesive whole.
+  Technology, Culture, & Spiritual
+  These days, our fundamental notions of existence, God is always evolving.
+}
