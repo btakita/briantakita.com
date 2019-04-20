86aa2d9f6af046a91373e3f2803f5a8faa96ba1c
Brian Takita <brian.takita@gmail.com>
2019-04-19 22:25:47 -0400

    bin/cp-nginx.sh
    	fix(@briantakita/bin): cp-nginx: errant 1
    
    packages/web/src/layout/Footer.html
    
    	feat(@briantakita/web): Footer: Navigation links as no JS fallback

--- bin/cp-nginx.sh
+++ bin/cp-nginx.sh
@@ -2,4 +2,3 @@
 NGINX_DIR="${NGINX_DIR:-/etc/nginx/}"
 sudo cp packages/web/nginx/briantakita.com.nginx $NGINX_DIR/sites-available/
 sudo ln -s $NGINX_DIR/sites-available/briantakita.com.nginx $NGINX_DIR/sites-enabled/
-1
