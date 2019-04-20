df6677aa9c361da8f023025c4b0c7860960d6d7c
Brian Takita <brian.takita@gmail.com>
2019-04-19 22:01:16 -0400

    bin/cp-nginx.sh
    	fix(@briantakita/bin): cp-nginx.sh error
    
    packages/web/nginx/briantakita.com.nginx
    
    	fix(@briantakita/web): briantakita.com.nginx config
    
    packages/web/src/template.html
    
    	feat(@briantakita/web): favicon.png
    
    + packages/web/static/favicon.png
    
    + packages/web/static/favicon.svg
    
    	source inkscape file

--- bin/cp-nginx.sh
+++ bin/cp-nginx.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-NGINX_DIR="${NGINX_DIR:-/etc/nginx/sites-available/}"
-sudo cp briantakita.com/briantakita.com.nginx $NGINX_DIR
+NGINX_DIR="${NGINX_DIR:-/etc/nginx/}"
+sudo cp packages/web/nginx/briantakita.com.nginx $NGINX_DIR/sites-available/
+sudo ln -s $NGINX_DIR/sites-available/briantakita.com.nginx $NGINX_DIR/sites-enabled/
+1
