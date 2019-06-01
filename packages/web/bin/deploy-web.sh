607da08e53d4cb8fbf7d0841927c91ae61d08f79
Brian Takita <brian.takita@gmail.com>
2019-06-01 13:43:40 -0400

    packages/web/bin/deploy-web.sh
    	popd only when pushd is successful

--- packages/web/bin/deploy-web.sh
+++ packages/web/bin/deploy-web.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-pushd packages/web
+pushd packages/web && SHOULD_POP=1
 npm run export
 cp now.json __sapper__/export
 now deploy __sapper__/export
 now alias briantakitacom.btak.now.sh briantakita.com
 now alias briantakitacom.btak.now.sh www.briantakita.com
-popd
+if [ $SHOULD_POP ]; then popd; fi
