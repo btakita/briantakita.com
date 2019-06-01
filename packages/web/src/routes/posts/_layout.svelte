09bc73d4f1a81af0c5d3b0095a8cab5917e3ffae
Brian Takita <brian.takita@gmail.com>
2019-06-01 13:36:29 -0400

    packages/ctx-core
    packages/web/package.json
    
    	↑ "@ctx-core/disqus": "^1.0.0"
    
    packages/web/src/routes/posts/_layout.svelte
    
    	∋ basename
    	∋ Disqus
    
    yarn.lock

--- packages/web/src/routes/posts/_layout.svelte
+++ packages/web/src/routes/posts/_layout.svelte
@@ -6,8 +6,10 @@
 	}
 </script>
 <script>
+	import { basename } from 'path'
 	import { __class__layout, __prepend__footer } from '../_layout/store'
 	import Date__Local from '@ctx-core/date/Date__Local.svelte'
+	import Disqus from '@ctx-core/disqus/Disqus.svelte'
 	import { __subheader } from '../_layout/store'
 	import { __frontmatter } from '@ctx-core/markdown/store'
 	export let path
@@ -26,25 +28,7 @@
 			<h2><a href="{path}">{title}</a></h2>
 		</header>
 		<slot></slot>
-		<div id="disqus_thread"></div>
-		<script type="text/javascript">
-			/**
-			 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-			 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-			var disqus_config = function () {
-				this.page.url = window.location.href
-				var hrefSegments = window.location.pathname.split('/')
-				var identifier = 'briantakita-' + hrefSegments[hrefSegments.length - 1]
-				this.page.identifier = identifier
-			};
-			(function () { // DON'T EDIT BELOW THIS LINE
-				var d = document, s = d.createElement('script')
-				s.src = 'https://briantakita.disqus.com/embed.js'
-				s.setAttribute('data-timestamp', +new Date());
-				(d.head || d.body).appendChild(s)
-			})()
-		</script>
-		<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+		<Disqus account="briantakita" identifier="briantakita-{basename(path)}"></Disqus>
   </div>
 </section>
 
