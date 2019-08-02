7b5075cfd8e4f8db8c71b26e6fee3309f206522d
Brian Takita <brian.takita@gmail.com>
2019-08-02 01:04:28 -0400

    feat: @briantakita/web: /presentations/*: navigation to home page & /presentations

---
+++ packages/_web/src/routes/presentations/_Nav__Presentation.svelte
@@ -0,0 +1,47 @@
+<script>
+	import FA_arrow_right_solid from '@ctx-core/fontawesome/ui/FA-arrow-right-solid.svelte'
+</script>
+
+<div class="layout__presentation">
+	<header>
+		<a href="/"><img src="/favicon.svg" title="BrianTakita.com" alt="BrianTakita.com"></a>
+		<FA_arrow_right_solid></FA_arrow_right_solid>
+		<a href="/presentations" title="Presentations" alt="Presentations">P</a>
+	</header>
+
+	<slot></slot>
+</div>
+
+<style type="text/scss" global>
+	@import "~@briantakita/css/variables";
+	.layout__presentation {
+		header {
+			position: fixed;
+			display: flex;
+			top: 0;
+			left: 0;
+			height: 1rem;
+			padding: 1rem;
+			z-index: 1;
+			> * {
+				display: block;
+			}
+			svg {
+				height: 1rem;
+				width: 1rem;
+				fill: $color__text;
+			}
+			a {
+				line-height: 1rem;
+				min-height: 1rem;
+				max-height: 1rem;
+				text-decoration: none;
+				vertical-align: top;
+				img {
+					height: 1rem;
+					width: 1rem;
+				}
+			}
+		}
+	}
+</style>
--- packages/_web/src/routes/presentations/reactive-tag-graph.svelte
+++ packages/_web/src/routes/presentations/reactive-tag-graph.svelte
@@ -1,5 +1,8 @@
 <script>
+	import Nav__Presentation from './_Nav__Presentation.svelte'
 	import Reactive_Tag_Graph_Presentation from '@briantakita/reactive-tag-graph-presentation/Reactive_Tag_Graph_Presentation.svelte'
 </script>
 
-<Reactive_Tag_Graph_Presentation></Reactive_Tag_Graph_Presentation>
+<Nav__Presentation>
+	<Reactive_Tag_Graph_Presentation></Reactive_Tag_Graph_Presentation>
+</Nav__Presentation>
