3d7b2f122b8a279b61647d956e7d66d6309b2766
Brian Takita <brian.takita@gmail.com>
2019-11-08 11:26:40 -0500

    new-archive.sh: *.md instead of *.svelte
    symlink-latest-archive.sh: previous--[number].md instead of previous-[number].md

--- packages/_web/bin/new-archive.sh
+++ packages/_web/bin/new-archive.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pushd $(dirname $0)/../src/routes/archive
-touch "$(date +%Y%m%d).svelte"
+touch "$(date +%Y%m%d).md"
 popd
--- packages/_web/bin/symlink-latest-archive.sh
+++ packages/_web/bin/symlink-latest-archive.sh
@@ -6,15 +6,15 @@ if [[ "$COUNT" -gt 1 ]]; then
 	ln -sf $(ls * | grep '^[0-9]\+\.md' | sort | tail -n2 | head -n1) previous.md
 fi
 if [[ "$COUNT" -gt 2 ]]; then
-	ln -sf $(ls * | grep '^[0-9]\+\.md' | sort | tail -n3 | head -n1) previous-2.md
+	ln -sf $(ls * | grep '^[0-9]\+\.md' | sort | tail -n3 | head -n1) previous--2.md
 fi
 if [[ "$COUNT" -gt 3 ]]; then
-	ln -sf $(ls * | grep '^[0-9]\+\.md' | sort | tail -n4 | head -n1) previous-3.md
+	ln -sf $(ls * | grep '^[0-9]\+\.md' | sort | tail -n4 | head -n1) previous--3.md
 fi
 if [[ "$COUNT" -gt 4 ]]; then
-	ln -sf $(ls * | grep '^[0-9]\+\.md' | sort | tail -n5 | head -n1) previous-4.md
+	ln -sf $(ls * | grep '^[0-9]\+\.md' | sort | tail -n5 | head -n1) previous--4.md
 fi
 if [[ "$COUNT" -gt 5 ]]; then
-	ln -sf $(ls * | grep '^[0-9]\+\.md' | sort | tail -n6 | head -n1) previous-5.md
+	ln -sf $(ls * | grep '^[0-9]\+\.md' | sort | tail -n6 | head -n1) previous--5.md
 fi
 popd
