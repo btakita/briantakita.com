3b4913bd83dcfe8c11d2958808fd79a8b0556743
Brian Takita <brian.takita@gmail.com>
2019-11-08 11:09:06 -0500

    @briantakita/highlight.js: + typescript support

--- packages/highlight.js/lib.js
+++ packages/highlight.js/lib.js
@@ -1,9 +1,12 @@
 import hljs from 'highlight.js/lib/highlight'
 import js__highlight from 'highlight.js/lib/languages/javascript'
+import ts__highlight from 'highlight.js/lib/languages/typescript'
 import json__highlight from 'highlight.js/lib/languages/json'
 import shell__highlight from 'highlight.js/lib/languages/shell'
 hljs.registerLanguage('javascript', js__highlight)
 hljs.registerLanguage('js', js__highlight)
+hljs.registerLanguage('typescript', ts__highlight)
+hljs.registerLanguage('ts', ts__highlight)
 hljs.registerLanguage('json', json__highlight)
 hljs.registerLanguage('shell', shell__highlight)
 export { hljs }
