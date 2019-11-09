49fd4838621d0cdc6d322bb92f47454ef0559dec
Brian Takita <brian.takita@gmail.com>
2019-11-08 20:37:23 -0500

    @briantakita/web:
    	deploy-web.sh: ∋ now --prod; - now alias
    	now.json: - alias

--- packages/_web/bin/deploy-web.sh
+++ packages/_web/bin/deploy-web.sh
@@ -2,7 +2,5 @@
 pushd packages/_web && SHOULD_POP=1
 npm run export
 cp now.json __sapper__/export
-now deploy __sapper__/export
-now alias briantakitacom.btak.now.sh briantakita.com
-now alias briantakitacom.btak.now.sh www.briantakita.com
+now --prod __sapper__/export
 if [ $SHOULD_POP ]; then popd; fi
